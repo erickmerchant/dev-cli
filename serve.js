50099331d4efd98d2ce81329f769248695471a3e
Erick Merchant <erickmerchant@gmail.com>
2018-11-24 01:29:02 -0800

    Work on compiling

--- serve.js
+++ serve.js
@@ -6,8 +6,19 @@ const assert = require('assert')
 const error = require('sergeant/error')
 const promisify = require('util').promisify
 const fs = require('fs')
+const resolve = require('browser-resolve')
+const babel = require('@babel/core')
+const babelPresetEnv = require('@babel/preset-env')
+const babelMinify = require('babel-preset-minify')
+const postcssMinify = require('cssnano')
+const postcss = require('postcss')
+const postcssPresetEnv = require('postcss-preset-env')
 const access = promisify(fs.access)
+const readFile = promisify(fs.readFile)
+const babelTransform = promisify(babel.transform)
 const createReadStream = fs.createReadStream
+const moduleMap = {}
+let moduleId = 0
 
 module.exports = (deps) => {
   assert.ok(deps.out)
@@ -16,12 +27,103 @@ module.exports = (deps) => {
 
   return async (args, cb = () => {}) => {
     const app = polka({
-      onError (err, req, res) {
-        error(err)
+      onError
+    })
 
-        res.writeHead(500, { 'content-type': 'text/plain; charset=utf-8' })
+    app.use(async (req, res, next) => {
+      if (req.path.endsWith('.mjs')) {
+        try {
+          res.writeHead(200, { 'content-type': 'application/javascript' })
 
-        res.end('')
+          let file
+
+          if (moduleMap[req.path] != null) {
+            file = path.join(args.directory, moduleMap[req.path])
+          } else {
+            file = path.join(args.directory, req.path)
+          }
+
+          await access(file, fs.constants.R_OK)
+
+          const code = await readFile(file, 'utf8')
+
+          const result = await babelTransform(code, {
+            sourceType: 'module',
+            sourceMaps: 'inline',
+            sourceFileName: req.path,
+            presets: [
+              [babelPresetEnv, { modules: false }],
+              babelMinify
+            ],
+            plugins: [
+              () => ({
+                visitor: {
+                  CallExpression (path) {
+                    if (path.node.callee.type !== 'Import') {
+                      return
+                    }
+
+                    const [source] = path.node.arguments
+                    if (source.type !== 'StringLiteral') {
+                      /* Should never happen */
+                      return
+                    }
+
+                    if (!source.value.startsWith('.') && !source.value.startsWith('/')) {
+                      source.value = getImportPath(source.value, args.directory, 'mjs')
+                    }
+                  },
+                  'ImportDeclaration|ExportNamedDeclaration|ExportAllDeclaration' (path) {
+                    const { source } = path.node
+
+                    // An export without a 'from' clause
+                    if (source === null) {
+                      return
+                    }
+
+                    if (!source.value.startsWith('.') && !source.value.startsWith('/')) {
+                      source.value = getImportPath(source.value, args.directory, 'mjs')
+                    }
+                  }
+                }
+              })
+            ]
+          })
+
+          res.end(result.code)
+        } catch (err) {
+          onError(err, req, res)
+        }
+      } else {
+        next()
+      }
+    })
+
+    app.use(async (req, res, next) => {
+      if (req.path.endsWith('.css')) {
+        try {
+          res.writeHead(200, { 'content-type': 'text/css' })
+
+          const file = path.join(args.directory, req.path)
+
+          await access(file, fs.constants.R_OK)
+
+          const code = await readFile(file, 'utf8')
+
+          const result = await postcss([
+            postcssPresetEnv(),
+            postcssMinify()
+          ]).process(code, {
+            from: req.path,
+            map: { inline: true }
+          })
+
+          res.end(result.css)
+        } catch (err) {
+          onError(err, req, res)
+        }
+      } else {
+        next()
       }
     })
 
@@ -32,20 +134,16 @@ module.exports = (deps) => {
 
     app.use(async (req, res, next) => {
       try {
-        const file = path.resolve(args.directory, 'index.html')
+        const file = path.join(args.directory, 'index.html')
 
         await access(file, fs.constants.R_OK)
 
         res.writeHead(200, { 'content-type': 'text/html' })
 
-        createReadStream(file, 'utf-8').pipe(res)
+        createReadStream(file, 'utf8').pipe(res)
       } catch (err) {
-        error(err)
+        onError(err, req, res)
       }
-
-      res.statusCode = 500
-
-      res.end()
     })
 
     app.listen(args.port, (err) => {
@@ -60,4 +158,27 @@ module.exports = (deps) => {
       }
     })
   }
+}
+
+function onError (err, req, res) {
+  error(err)
+
+  res.writeHead(500, { 'content-type': 'text/plain' })
+
+  res.end('')
+}
+
+function getImportPath (source, directory, ext) {
+  const result = path.relative(path.resolve(directory), resolve.sync(source))
+  const preExisting = Object.keys(moduleMap).find((id) => moduleMap[id] === result)
+
+  if (preExisting) {
+    return preExisting
+  }
+
+  const id = `/m/${moduleId++}.${ext}`
+
+  moduleMap[id] = result
+
+  return id
 }
