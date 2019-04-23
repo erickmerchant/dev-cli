2ff42c74aaefe007649a8fa9a24e415712aa5e2e
Erick Merchant <erickmerchant@gmail.com>
2019-04-22 21:43:59 -0700

    Upgrade sergeant

--- cli.js
+++ cli.js
@@ -1,57 +1,63 @@
 #!/usr/bin/env node
 
-const command = require('sergeant')
+const {command, start} = require('sergeant')('dev')
 const serve = require('./serve.js')
 const cache = require('./cache.js')
 
-command('dev', 'run a development server or cache responses, automatically running babel and postcss', ({command}) => {
-  command('serve', ({option, parameter}) => {
-    parameter('src', {
-      description: 'where to serve files from',
-      required: true,
-      type(val) {
-        return val
-      }
-    })
-
-    option('port', {
-      description: 'the port to listen at',
-      type(val = 3000) {
-        return val ? Number(val) : null
-      },
-      alias: 'p'
-    })
-
-    option('dev', {
-      description: 'run in dev mode',
-      alias: 'd'
-    })
-
-    return (args) => serve({console})(args)
+command(['serve'], ({option, parameter}) => {
+  parameter({
+    name: 'src',
+    description: 'where to serve files from',
+    required: true,
+    type(val) {
+      return val
+    }
   })
 
-  command('cache', ({option, parameter}) => {
-    parameter('src', {
-      description: 'where to cache files from',
-      required: true,
-      type(val) {
-        return val
-      }
-    })
-
-    parameter('dist', {
-      description: 'where to cache files to',
-      required: true,
-      type(val) {
-        return val
-      }
-    })
-
-    option('dev', {
-      description: 'run in dev mode',
-      alias: 'd'
-    })
-
-    return (args) => cache({console})(args)
+  option({
+    name: 'port',
+    description: 'the port to listen at',
+    type(val = 3000) {
+      return val ? Number(val) : null
+    },
+    alias: 'p'
   })
-})(process.argv.slice(2))
+
+  option({
+    name: 'dev',
+    description: 'run in dev mode',
+    alias: 'd'
+  })
+
+  return (args) => serve({console})(args)
+})
+
+command(['cache'], ({option, parameter}) => {
+  parameter({
+    name: 'src',
+    description: 'where to cache files from',
+    required: true,
+    type(val) {
+      return val
+    }
+  })
+
+  parameter({
+    name: 'dist',
+    description: 'where to cache files to',
+    required: true,
+    type(val) {
+      return val
+    }
+  })
+
+  option({
+    name: 'dev',
+    description: 'run in dev mode',
+    alias: 'd'
+  })
+
+  return (args) => cache({console})(args)
+})
+
+start(process.argv.slice(2))
